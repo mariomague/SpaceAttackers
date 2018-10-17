9c492c936d5809dfabd8c17a099146f1767b110d
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2018-10-17 08:22:01 +0100

    Initial commit

---
+++ Assets/Proyect/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 3f;
+    private float limit;
+
+
+	// Use this for initialization
+	void Start () {
+        limit = (((Screen.width / 100f) / 2f) / Camera.main.aspect);
+        Debug.Log(limit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //move player
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * Input.GetAxis("Horizontal") * speed;
+        //bounds
+        if(transform.position.x > limit)
+        {
+            transform.position = new Vector3(limit,transform.position.y,transform.position.z);
+        }
+        else if(transform.position.x < -limit)
+        {
+            transform.position = new Vector3(-limit, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+	}
+}
